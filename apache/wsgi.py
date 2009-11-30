1bf852ae1958f6c59332e7e8f1e269b33a1f24aa
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2009-12-01 10:41:00 +1100

    Added simple WSGI bridge for mod_wsgi.

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -1,4 +1,39 @@
-def handle_request(environ, start_response):
+from apache.httpd import *
+from apache.http_protocol import *
 
-    start_response('501 Not Implemented', [])
-    return ['Not Implemented']
+import sys
+import imp
+import md5
+
+_cache = {}
+
+def handle_request(environ, start_response):
+    filename = environ['SCRIPT_FILENAME']
+
+    if filename not in _cache:
+        label = '_ap_' + md5.new(filename).hexdigest()
+        module = imp.new_module(label)
+        module.__file__ = filename
+        execfile(filename, module.__dict__)
+        sys.modules[label] = module
+        _cache[filename] = module
+    else:
+        module = _cache[filename]
+
+    r = request_rec(environ['apache.request_rec'])
+
+    if not hasattr(module, 'handler'):
+        r.status = 404
+        ap_rflush(r)
+
+        start_response('200 OK', [])
+        return []
+
+    handler = getattr(module, 'handler')
+
+    handler(r)
+
+    ap_rflush(r)
+
+    start_response('200 OK', [])
+    return ['']
